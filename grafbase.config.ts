6f82cba14789e7b219bc8d02edb8d94c0f4d5c5e
erisk405 <133034175+erisk405@users.noreply.github.com>
2024-03-22 00:45:47 +0700

    FIXGRAHP

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,6 +1,23 @@
 import { graph, config } from '@grafbase/sdk'
 const g = graph.Standalone()
 
+const user = g.type('User', {
+  name: g.string(),
+  email: g.string(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+})
+const project = g.type('Project',{
+  title: g.string(),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string(),
+})
+
 export default config({
   graph: g,
 })
