59a955974c9ef6bca0b9568a7947fc135b2b7de2
erisk405 <133034175+erisk405@users.noreply.github.com>
2024-03-22 01:05:46 +0700

    agian

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,26 +1,23 @@
-import { g, config, auth } from '@grafbase/sdk';
+import { graph, config } from '@grafbase/sdk'
+const g = graph.Standalone()
 
-const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 100 }),
-  email: g.string().unique(),
+const user = g.type('User', {
+  name: g.string(),
+  email: g.string(),
   avatarUrl: g.url(),
-  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  description: g.string().optional(),
   githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(), 
-  projects: g.relation(() => Project).list().optional(),
+  linkedInUrl: g.url().optional(),
 })
-
-const Project = g.model('Project', {
-  title: g.string().length({ min: 3 }),
-  description: g.string(), 
+const project = g.type('Project',{
+  title: g.string(),
+  description: g.string(),
   image: g.url(),
-  liveSiteUrl: g.url(), 
-  githubUrl: g.url(), 
-  category: g.string().search(),
-  createdBy: g.relation(() => User),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string(),
 })
 
-// finally we export the default config
 export default config({
-  schema: g,
+  graph: g,
 })
